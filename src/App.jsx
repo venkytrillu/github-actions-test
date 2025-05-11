27b99bff3a79b3ff697fa872eb54589dd8a6fe20
Venky <ganjivenkatesh.i49@gmail.com>
2025-05-11 18:33:42 +0530

    6th commit [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
-
+// testing comment
 function App() {
   return (
     <>
       <header>
-        <div id="logo-img">
+        <div id="logo-img"> 
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
